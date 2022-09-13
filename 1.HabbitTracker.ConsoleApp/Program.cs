fa997b2ed65e4d25df8e25b941ebbebd8df75200
gadadgo <kretovich3@gmail.com>
2022-09-13 12:13:56 +0200

    Line 1 to 7

--- 1.HabbitTracker.ConsoleApp/Program.cs
+++ 1.HabbitTracker.ConsoleApp/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("These are the most common data types:");
+Console.WriteLine(1+ " - String, example" + " - Good");
+Console.WriteLine(2+ " - Char, example " + 'G');
+Console.WriteLine(3+ " - Int, example " + 6);
+Console.WriteLine(4+ " - Decimal");
+Console.WriteLine(5+ " - Bool");
+Console.ReadLine();
