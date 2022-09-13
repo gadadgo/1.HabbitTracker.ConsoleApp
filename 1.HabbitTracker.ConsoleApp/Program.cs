e30f9f7636ff9970e98ded27ea5673cec906072a
gadadgo <kretovich3@gmail.com>
2022-09-13 23:35:33 +0200

    Part6

--- 1.HabbitTracker.ConsoleApp/Program.cs
+++ 1.HabbitTracker.ConsoleApp/Program.cs
@@ -1,17 +1,7 @@
-﻿string name = "Bob";
-int index = 1;
-char initail = 'B';
-decimal height = 1.88m;
-bool alive = true;
+﻿//Part 6
 
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+var date = DateTime.UtcNow;
 
-var myParagraph = @$"These are the most common data types:
-{index++}  - String, example {name}
-{index++}  - Char, example {initail}
-{index++}  - Int, example {6}
-{index++}  - Decimal, example {height}
-{index++}  - Bool, example {alive}";
-
-Console.WriteLine(myParagraph);
-Console.ReadLine();
-
+Console.WriteLine($"Hello {name} it is {date} , welcome the the math game");
