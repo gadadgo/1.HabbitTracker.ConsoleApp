cc79e84c1233943b995c709ada51cff8197cc380
gadadgo <kretovich3@gmail.com>
2022-09-13 23:42:04 +0200

    intro

--- 1.HabbitTracker.ConsoleApp/Program.cs
+++ 1.HabbitTracker.ConsoleApp/Program.cs
@@ -3,5 +3,13 @@
 Console.WriteLine("Please enter your name");
 string name = Console.ReadLine();
 var date = DateTime.UtcNow;
+string introduction = $@"hat game would you like to play? Choose below.
+A - Addition
+S - Subtraction
+D - Division
+M - Multiplication
+Q - Quit the program";
 
 Console.WriteLine($"Hello {name} it is {date} , welcome the the math game");
+Console.WriteLine(introduction);    
+
