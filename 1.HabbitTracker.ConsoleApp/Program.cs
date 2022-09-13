9c872b009dde95ffcc81bc158563276bdca1b7d8
gadadgo <kretovich3@gmail.com>
2022-09-13 23:22:43 +0200

    String interpolated

--- 1.HabbitTracker.ConsoleApp/Program.cs
+++ 1.HabbitTracker.ConsoleApp/Program.cs
@@ -1,15 +1,15 @@
 ﻿string name = "Bob";
-int age = 1;
+int index = 1;
 char initail = 'B';
 decimal height = 1.88m;
 bool alive = true;
 
 
 Console.WriteLine("These are the most common data types:");
-Console.WriteLine(age++ + " - String, example " + name);
-Console.WriteLine(age++ + " - Char, example " + initail);
-Console.WriteLine(age++ + " - Int, example " + 6);
-Console.WriteLine(age++ + " - Decimal, example " + height);
-Console.WriteLine(age++ + " - Bool, example " + true);
+Console.WriteLine($"{index++}  - String, example {name}");
+Console.WriteLine($"{index++}  - Char, example {initail}");
+Console.WriteLine($"{index++}  - Int, example {6}");
+Console.WriteLine($"{index++}  - Decimal, example {height}");
+Console.WriteLine($"{index++}  - Bool, example {true}");
 Console.ReadLine();
 
