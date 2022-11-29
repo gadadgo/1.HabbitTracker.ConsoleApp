d6315cf299fc52aba82a98c71e957a69ddc323b5
gadadgo <kretovich3@gmail.com>
2022-11-29 13:09:45 +0100

    gameENGclass

--- 1.HabbitTracker.ConsoleApp/GameEngine.cs
+++ 1.HabbitTracker.ConsoleApp/GameEngine.cs
@@ -8,5 +8,150 @@ namespace _1.HabbitTracker.ConsoleApp
 {
     internal class GameEngine
     {
+        internal void MultiplicationGame()
+        {
+            int firstNumber;
+            int secondNumber;
+
+            int score = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Multiplication game");
+                var random = new Random();
+                firstNumber = random.Next(1, 10);
+                secondNumber = random.Next(1, 10);
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber * secondNumber)
+                {
+                    Console.WriteLine("The answer is correct, press 'Enter' button to continue");
+                    Console.ReadLine();
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("The answer is wrong, press 'Enter' button to continue");
+                    Console.ReadLine();
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine($"Game over, your score is {score}");
+                }
+            }
+            AddToHistory(score, "Multiplication");
+        }
+
+        internal void DivisionGame()
+        {
+            Console.WriteLine("Division game");
+            var score = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Clear();
+                var divisionNumbers = GetDivisionNumbers();
+                var firstNumber = divisionNumbers[0];
+                var secondNumber = divisionNumbers[1];
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber / secondNumber)
+                {
+                    Console.WriteLine("The answer is correct, press 'Enter' to continue");
+                    Console.ReadLine();
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("The answer is wrong, press 'Enter' to continue");
+                    Console.ReadLine();
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine($"Game over! Your score is {score}");
+                }
+            }
+            AddToHistory(score, "Division");
+        }
+
+        internal void SubtractinGame()
+        {
+            Console.WriteLine("Subtraction game");
+            int firstNumber;
+            int secondNumber;
+
+            int score = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Clear();
+                var random = new Random();
+                firstNumber = random.Next(1, 10);
+                secondNumber = random.Next(1, 10);
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber - secondNumber)
+                {
+                    Console.WriteLine("The answer is correct, press 'Enter' to continue");
+                    Console.ReadLine();
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("The answer is wrong, press 'Enter' to continue");
+                    Console.ReadLine();
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine($"Game over, your score is {score}");
+                }
+            }
+            AddToHistory(score, "Subtraction");
+        }
+
+        internal void AdditionGame()
+        {
+            Console.WriteLine("Addition game is selected");
+
+            int firstNumber;
+            int secondNumber;
+
+            int score = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Clear();
+                var random = new Random();
+                firstNumber = random.Next(1, 10);
+                secondNumber = random.Next(1, 10);
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                var result = Console.ReadLine();
+                if (int.Parse(result) == firstNumber + secondNumber)
+                {
+                    Console.WriteLine("The answer is correct, press 'Enter' to continue");
+                    Console.ReadLine();
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("The answer is wrong, press 'Enter' to continue");
+                    Console.ReadLine();
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine($"Game over, your score is {score}");
+                }
+            }
+
+            AddToHistory(score, "Addition");
+        }
+
+        internal void Exit()
+        {
+            Console.WriteLine("Goodbye");
+            Environment.Exit(2);
+        }
+
     }
 }
--- 1.HabbitTracker.ConsoleApp/Program.cs
+++ 1.HabbitTracker.ConsoleApp/Program.cs
@@ -19,165 +19,17 @@ string GetName()
     return name;
 }
 
-
-
 void GetGames()
 {
     Console.Clear();
     Console.WriteLine("Games history");
-    foreach(var game in games)
+    foreach (var game in games)
     {
         Console.WriteLine(game);
     }
     Console.WriteLine("Press 'Enter' to continue");
     Console.ReadLine();
 }
-
-void MultiplicationGame()
-{
-    int firstNumber;
-    int secondNumber;
-
-    int score = 0;
-
-    for (int i = 0; i < 6; i++)
-    {
-        Console.Clear();
-        Console.WriteLine("Multiplication game");
-        var random = new Random();
-        firstNumber = random.Next(1, 10);
-        secondNumber = random.Next(1, 10);
-        Console.WriteLine($"{firstNumber} * {secondNumber}");
-        var result = Console.ReadLine();
-        if (int.Parse(result) == firstNumber * secondNumber)
-        {
-            Console.WriteLine("The answer is correct, press 'Enter' button to continue");
-            Console.ReadLine();
-            score++;
-        }
-        else
-        {
-            Console.WriteLine("The answer is wrong, press 'Enter' button to continue");
-            Console.ReadLine();
-        }
-        if (i == 5)
-        {
-            Console.WriteLine($"Game over, your score is {score}");
-        }
-    }
-    AddToHistory(score, "Multiplication");
-}
-
-void DivisionGame()
-{
-    Console.WriteLine("Division game");
-    var score = 0;
-    for (int i = 0; i < 6; i++)
-    {
-        Console.Clear();
-        var divisionNumbers = GetDivisionNumbers();
-        var firstNumber = divisionNumbers[0];
-        var secondNumber = divisionNumbers[1];
-        
-        Console.WriteLine($"{firstNumber} / {secondNumber}");
-        var result = Console.ReadLine();
-        if (int.Parse(result) == firstNumber / secondNumber)
-        {
-            Console.WriteLine("The answer is correct, press 'Enter' to continue");
-            Console.ReadLine();
-            score++;
-        }
-        else
-        {
-            Console.WriteLine("The answer is wrong, press 'Enter' to continue");
-            Console.ReadLine();
-        }
-        if (i == 5)
-        {
-            Console.WriteLine($"Game over! Your score is {score}");
-        }
-    }
-    AddToHistory(score, "Division");
-}
-
-void SubtractinGame()
-{
-    Console.WriteLine("Subtraction game");
-    int firstNumber;
-    int secondNumber;
-
-    int score = 0;
-
-    for (int i = 0; i < 6; i++)
-    {
-        Console.Clear();
-        var random = new Random();
-        firstNumber = random.Next(1, 10);
-        secondNumber = random.Next(1, 10);
-        Console.WriteLine($"{firstNumber} - {secondNumber}");
-        var result = Console.ReadLine();
-        if (int.Parse(result) == firstNumber - secondNumber)
-        {
-            Console.WriteLine("The answer is correct, press 'Enter' to continue");
-            Console.ReadLine();
-            score++;
-        }
-        else
-        {
-            Console.WriteLine("The answer is wrong, press 'Enter' to continue");
-            Console.ReadLine();
-        }
-        if (i == 5)
-        {
-            Console.WriteLine($"Game over, your score is {score}");
-        }
-    }
-    AddToHistory(score, "Subtraction");
-}
-
-void AdditionGame()
-{
-    Console.WriteLine("Addition game is selected");
-
-    int firstNumber;
-    int secondNumber;
-
-    int score = 0;
-
-    for (int i = 0; i < 6; i++)
-    {
-        Console.Clear();
-        var random = new Random();
-        firstNumber = random.Next(1, 10);
-        secondNumber = random.Next(1, 10);
-        Console.WriteLine($"{firstNumber} + {secondNumber}");
-        var result = Console.ReadLine();
-        if (int.Parse(result) == firstNumber + secondNumber)
-        {
-            Console.WriteLine("The answer is correct, press 'Enter' to continue");
-            Console.ReadLine();
-            score++;
-        }
-        else
-        {
-            Console.WriteLine("The answer is wrong, press 'Enter' to continue");
-            Console.ReadLine();
-        }
-        if (i == 5)
-        {
-            Console.WriteLine($"Game over, your score is {score}");
-        }
-    }
-
-    AddToHistory(score, "Addition");
-}
-
-void Exit()
-{
-    Console.WriteLine("Goodbye");
-    Environment.Exit(2);
-}
-
 void AddToHistory(int gameScore, string gameType)
 {
     games.Add($"{DateTime.Now} - {gameType}: {gameScore}");
